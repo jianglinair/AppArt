6b866b5c879112949e41b8e4c7bbe470ee96b8f8
jianglin <jianglin@qiyi.com>
2017-08-13 14:28:22 +0800

    Serializable: Java, 推荐用于文件,网络传输序列化 Parcelable: Android, 推荐用于内存序列化
    Change-Id: Iaad2b5839ac8bb70039b42d1b53900745c20f8d8

---
+++ chapter2/src/main/java/com/lin/jiang/appart/User.java
@@ -0,0 +1,16 @@
+package com.lin.jiang.appart;
+
+import java.io.Serializable;
+
+
+/**
+ * Created by jianglin on 17-8-13.
+ */
+
+public class User implements Serializable {
+    private static final long serialVersionUID = 1487578443538917884L;
+
+    public int userId;
+    public String userName;
+    public boolean isMale;
+}
