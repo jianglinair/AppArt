9a77836ea84d5db4725e394e805901a21974483f
jianglin <jianglin@qiyi.com>
2017-08-13 16:01:26 +0800

    AIDL 1) 声明 AIDL 接口文件 Test.aidl{void test();} 2) 生成 java 接口文件 Test.java 3) // Test.java public interface Test extends IInterface {
        void test();
    
        public static abstract class Stub extends Binder implements Test {
    
             public static Test asInterface(android.os.IBinder obj) {...}
    
             public boolean onTransact(int code, Parcel data, Parcel reply, int flags) {...}
    
             private static class Proxy implements Test {...void test()...}
        }
    }
    
    Stub 即是 Binder对象
    
    asInterface(): 用于将服务端的 Binder 对象转换成客户端的 AIDL 接口类型的对象，如果客户端和服务端位于同一个进程，则返回服务端的 Stub 对象本身，否则返回 Stub.proxy 对象
    
    Proxy 运行在客户端，客户端调 test() 实际上是委托 proxy.test()，然后由 Stub 的 transact() 方法将客户端调用的 test() 分发至实际实现接口的 Service
    
    Change-Id: Ieda8e48bc08da28bf6c5d18c7af89e32e686477b

---
+++ chapter2/src/main/aidl/com/lin/jiang/appart/aidl/Book.aidl
@@ -0,0 +1,4 @@
+// Book.aidl
+package com.lin.jiang.appart.aidl;
+
+parcelable Book;
---
+++ chapter2/src/main/java/com/lin/jiang/appart/aidl/Book.java
@@ -0,0 +1,48 @@
+package com.lin.jiang.appart.aidl;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * Created by jianglin on 17-8-13.
+ */
+
+public class Book implements Parcelable {
+
+    public int bookId;
+    public String bookName;
+
+    public Book(int bookId, String bookName) {
+        this.bookId = bookId;
+        this.bookName = bookName;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel out, int flags) {
+        out.writeInt(bookId);
+        out.writeString(bookName);
+    }
+
+    public static final Creator<Book> CREATOR = new Creator<Book>() {
+        @Override
+        public Book createFromParcel(Parcel in) {
+            return new Book(in);
+        }
+
+        @Override
+        public Book[] newArray(int size) {
+            return new Book[size];
+        }
+    };
+
+    private Book(Parcel in) {
+        bookId = in.readInt();
+        bookName = in.readString();
+    }
+
+}
