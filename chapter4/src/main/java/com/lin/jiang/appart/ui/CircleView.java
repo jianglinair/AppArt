de9c1bdd5d257a48e81242c9a4c7a558fb86b5da
jianglin <jianglin@qiyi.com>
2017-08-22 21:23:43 +0800

    继承 View 重写 onDraw 方法
    注意:
    1) 支持 wrap_content;
    2) 处理 padding;
    3) 对外提供自定义属性
    
    Change-Id: I692524059857a7345b4c4a5ebd3db0b4434de3ce

---
+++ chapter4/src/main/java/com/lin/jiang/appart/ui/CircleView.java
@@ -0,0 +1,53 @@
+package com.lin.jiang.appart.ui;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by jianglin on 17-8-22.
+ * <p>
+ * 继承 View 重写 onDraw 方法<br/>
+ * 注意 1) 支持 wrap_content; 2) 处理 padding; 3) 对外提供自定义属性
+ */
+
+public class CircleView extends View {
+
+    private static final String TAG = "CircleView";
+
+    private int mColor = Color.RED;
+    private Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
+    public CircleView(Context context) {
+        super(context);
+        init();
+    }
+
+    public CircleView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public CircleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        mPaint.setColor(mColor);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        int width = getWidth();
+        int height = getHeight();
+        int radius = Math.min(width, height) / 2;
+        canvas.drawCircle(width / 2, height / 2, radius, mPaint);
+    }
+}
