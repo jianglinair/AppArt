21006f5ce3a45d64fe743491f23603c0522ecd1f
jianglin <jianglin@qiyi.com>
2017-08-22 21:47:21 +0800

    继承 View, 重写 onDraw 方法
        注意:
        1) 支持 wrap_content;
        2) 处理 padding;
        3) 对外提供自定义属性
    
    1）重写 onMeassure 以支持 wrap_content
    2）改写 onDraw 以支持 padding
    
    Change-Id: I57d4c82529cb7557b2c79cf31a1aa25b8e0a8fa0

--- chapter4/src/main/java/com/lin/jiang/appart/ui/CircleView.java
+++ chapter4/src/main/java/com/lin/jiang/appart/ui/CircleView.java
@@ -41,13 +41,47 @@ public class CircleView extends View {
         mPaint.setColor(mColor);
     }
 
+    /**
+     * 重写 onMeasure 以支持 wrap_content 属性
+     * <p>
+     * 本例指定属性为 wrap_content 时设置默认宽高200px
+     *
+     * @param widthMeasureSpec
+     * @param heightMeasureSpec
+     */
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int widthSpecMode = MeasureSpec.getMode(widthMeasureSpec);
+        int widthSpecSize = MeasureSpec.getSize(widthMeasureSpec);
+        int heightSpecMode = MeasureSpec.getMode(heightMeasureSpec);
+        int heightSpecSize = MeasureSpec.getSize(heightMeasureSpec);
+
+        if (widthSpecMode == MeasureSpec.AT_MOST && heightSpecMode == MeasureSpec.AT_MOST) {
+            setMeasuredDimension(200, 200); // setMeasuredDimension 会设置 View 宽高的测量值
+        } else if (widthSpecMode == MeasureSpec.AT_MOST) {
+            setMeasuredDimension(200, heightSpecSize);
+        } else if (heightSpecMode == MeasureSpec.AT_MOST) {
+            setMeasuredDimension(widthSpecSize, 200);
+        }
+    }
+
+    /**
+     * 添加 padding 处理
+     *
+     * @param canvas
+     */
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        int width = getWidth();
-        int height = getHeight();
+        int paddingLeft = getPaddingLeft();
+        int paddingTop = getPaddingTop();
+        int paddingRight = getPaddingRight();
+        int paddingBottom = getPaddingBottom();
+        int width = getWidth() - paddingLeft - paddingRight;
+        int height = getHeight() - paddingTop - paddingBottom;
         int radius = Math.min(width, height) / 2;
-        canvas.drawCircle(width / 2, height / 2, radius, mPaint);
+        canvas.drawCircle(paddingLeft + width / 2, paddingTop + height / 2, radius, mPaint);
     }
 }
