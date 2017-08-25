2c1ef5d7ff9e8739bbaaa5950f2f7babb668b960
jianglin <jianglin@qiyi.com>
2017-08-25 09:22:39 +0800

    init chapter8
        Window<PhoneWindow>(DecorView) <----- WindowManagerService
        ViewRoot<ViewRootImpl>
        WindowManager<WindowManagerImpl>
    
    Change-Id: I370e0d9e97e77ad7105e48c55a1ccd8e3b689cd9

---
+++ chapter8/src/main/java/com/lin/jiang/appart/MainActivity.java
@@ -0,0 +1,25 @@
+package com.lin.jiang.appart;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onButtonClick(View v) {
+        if (v.getId() == R.id.button1) {
+            Intent intent = new Intent(this, TestActivity.class);
+            startActivity(intent);
+        } else if (v.getId() == R.id.button2) {
+            Intent intent = new Intent(this, DemoActivity.class);
+            startActivity(intent);
+        }
+    }
+}
